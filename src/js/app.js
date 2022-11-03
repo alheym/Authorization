5ac981ff733f2accdf739d5e23e44ae1ccc7d636
alheym <alheym.mw@gmail.com>
2022-11-03 10:14:40 +0300

    creating an authorization project

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../css/style.css';
+
+import UI from './config/ui.config';
+import validate from './helpers/validate';
+
+const { form, inputPassword, inputEmail } = UI;
+const inputs = [inputEmail, inputPassword];
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    onSubmit();
+})
+
+function onSubmit() {
+    
+}
